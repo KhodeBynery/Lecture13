cd33f759c4a167a6d95b2cb1e2ea55720728e9bb
Khode <Khode@hub.com>
2019-01-02 19:26:35 -0400

    Lecture13

--- part3/app.js
+++ part3/app.js
@@ -1,44 +1,45 @@
 (function () {
-  'use strict';
+'use strict';
 
-  angular.module('MsgApp', [])
-  .controller('MsgController', MsgController)
-  .filter('loves', lovesFilter);
-  .filter('truth', truthFilter);
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController)
+.filter('loves', LovesFilter)
+.filter('truth', TruthFilter);
 
-  MsgController.$inject = ['$scope', 'lovesFilter'];
-  function MsgController($scope, lovesFilter) {
-    $scope.stateOfBeing = "hungry";
+MsgController.$inject = ['$scope', 'lovesFilter'];
+function MsgController($scope, lovesFilter) {
+  $scope.stateOfBeing = "hungry";
 
-    $scope.sayMessage = function () {
-      var msg = "Yaakov likes to eat healthy snacks at night!";
-      return msg;
-    };
+  $scope.sayMessage = function () {
+    var msg = "Yaakov likes to eat healthy snacks at night!";
+    return msg;
+  };
 
-    $scope.sayLovesMessage = function () {
-      var msg = "Yaakov likes to eat healthy snacks at night!";
-      msg = lovesFilter(msg)
-      return msg;
-    };
+  $scope.sayLovesMessage = function () {
+    var msg = "Yaakov likes to eat healthy snacks at night!";
+    msg = lovesFilter(msg)
+    return msg;
+  };
 
-    $scope.feedYaakov = function () {
-      $scope.stateOfBeing = "fed";
-    };
+  $scope.feedYaakov = function () {
+    $scope.stateOfBeing = "fed";
+  };
+}
+
+function LovesFilter() {
+  return function (input) {
+    input = input || "";
+    input = input.replace("likes", "loves");
+    return input;
+  };
+}
+
+function TruthFilter() {
+  return function (input, target, replace) {
+    input = input || "";
+    input = input.replace(target, replace);
+    return input;
   }
+}
 
-  function lovesFilter() {
-    return function (input) {
-      input = input || "";
-      input = input.replace("likes", "loves");
-      return input;
-    };
-  }
-
-  function truthFilter() {
-    return function (input, target, replace) {
-      input = input || "";
-      input = input.replace(target, replace);
-      return input;
-    };
-  }
 })();
